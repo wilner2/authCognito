18e5b9c2fb46b144578198362527a2b8dfb10b40
wilner2 <wilnerbrum@gmail.com>
2024-04-02 15:24:03 -0300

    test: return error if no email provided on SignUpcontroller

--- tests/presentation/signUp.spec.ts
+++ tests/presentation/signUp.spec.ts
@@ -4,12 +4,12 @@ describe("signUp Controller", () => {
   test("should return 400 if no email is provided", () => {
     const sut = new SignUpController();
     const request = {
-      email: "any_test@example.com",
       password: "any_password",
       confirmPassword: "any_password",
     };
 
     const response = sut.handle(request);
     expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new Error("Missing param: email"));
   });
 });
