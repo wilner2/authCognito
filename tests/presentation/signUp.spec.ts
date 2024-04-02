326c5aea20bd38a49ce407221b3842f3397a9e18
wilner2 <wilnerbrum@gmail.com>
2024-04-02 14:07:52 -0300

    test: should return 400 if no email is provided on SignUpController

---
+++ tests/presentation/signUp.spec.ts
@@ -0,0 +1,19 @@
+
+import { SignUpController } from '@/presentation/signUp'
+
+describe('signUp Controller', () => {
+    test('should return 400 if no email is provided', () => {
+
+        const sut = new SignUpController();
+        const request = {
+            email: 'any_test@example.com',
+            password: 'any_password',
+            confirmPassword: 'any_password'
+        }
+
+        const response = sut.handle(request)
+        expect(response.statusCode).toBe(400)
+
+    });
+
+});
