b8df14070d895aed278c9f3f6e4a143c062efef1
wilner2 <wilnerbrum@gmail.com>
2024-04-02 15:26:19 -0300

    feat: should return 400 if no password is provided on SignUpcontroller

--- src/presentation/signUp.ts
+++ src/presentation/signUp.ts
@@ -6,5 +6,11 @@ export class SignUpController {
         body: new Error("Missing param: email"),
       };
     }
+    if (!request.password) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: password"),
+      };
+    }
   }
 }
