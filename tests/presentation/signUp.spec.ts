e4d1f04de61d2ce2cd80ba892a32868d95a39c08
wilner2 <wilnerbrum@gmail.com>
2024-04-02 15:26:26 -0300

    test: should return 400 if no password is provided on SignUpcontroller

--- tests/presentation/signUp.spec.ts
+++ tests/presentation/signUp.spec.ts
@@ -12,4 +12,16 @@ describe("signUp Controller", () => {
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual(new Error("Missing param: email"));
   });
+
+  test("should return 400 if no password is provided", () => {
+    const sut = new SignUpController();
+    const request = {
+      email: "any_email@email.com",
+      confirmPassword: "any_password",
+    };
+
+    const response = sut.handle(request);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new Error("Missing param: password"));
+  });
 });
