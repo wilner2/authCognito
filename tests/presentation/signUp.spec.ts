dbc012e88318d3e38dd9a0f364eb736de641e210
wilner2 <wilnerbrum@gmail.com>
2024-04-03 20:35:38 -0300

    test: refactor validation on SignUpController

--- tests/presentation/signUp.spec.ts
+++ tests/presentation/signUp.spec.ts
@@ -1,27 +1,32 @@
 import { SignUpController } from "@/presentation/signUp";
+import { ParamsIsMissing } from "@/presentation/helpers/excepetions";
 
 describe("signUp Controller", () => {
   test("should return 400 if no email is provided", () => {
     const sut = new SignUpController();
     const request = {
-      password: "any_password",
-      confirmPassword: "any_password",
+      body: {
+        password: "any_password",
+        confirmPassword: "any_password",
+      },
     };
 
     const response = sut.handle(request);
     expect(response.statusCode).toBe(400);
-    expect(response.body).toEqual(new Error("Missing param: email"));
+    expect(response.body).toEqual(new ParamsIsMissing("email"));
   });
 
   test("should return 400 if no password is provided", () => {
     const sut = new SignUpController();
     const request = {
-      email: "any_email@email.com",
-      confirmPassword: "any_password",
+      body: {
+        email: "any_email@email.com",
+        confirmPassword: "any_password",
+      },
     };
 
     const response = sut.handle(request);
     expect(response.statusCode).toBe(400);
-    expect(response.body).toEqual(new Error("Missing param: password"));
+    expect(response.body).toEqual(new ParamsIsMissing("password"));
   });
 });
