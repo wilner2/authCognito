ef4b780910a9420c3c8fbcb530ea7f13e56d6b11
wilner2 <wilnerbrum@gmail.com>
2024-04-04 09:33:54 -0300

    test:  return 200 if user created successfully on SignUpController

--- tests/presentation/signUp.spec.ts
+++ tests/presentation/signUp.spec.ts
@@ -29,4 +29,20 @@ describe("signUp Controller", () => {
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual(new ParamsIsMissing("password"));
   });
+
+  test("should return 200 if user created successfully", () => {
+    const sut = new SignUpController();
+    const request = {
+      body: {
+        email: "any_email@email.com",
+        password: "any_password",
+        confirmPassword: "any_password",
+      },
+    };
+    const response = sut.handle(request);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(
+      `User ${request.body.email} created successfully`
+    );
+  });
 });
