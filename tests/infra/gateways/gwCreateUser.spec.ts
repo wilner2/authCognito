c3bae491fa9b4c92b6f677fdd184a3bf628e7b21
wilner2 <wilnerbrum@gmail.com>
2024-04-04 17:15:26 -0300

    test: call updateHasher corretly on GWCreateUser

--- tests/infra/gateways/gwCreateUser.spec.ts
+++ tests/infra/gateways/gwCreateUser.spec.ts
@@ -8,21 +8,33 @@ describe("GWCreateUser", () => {
   const dataCreateUser = {
     email: "any_email@example.com",
     password: "any_email",
+    userName: "any_userName",
   };
 
   beforeAll(() => {
     encryptStub = mock<Encrypt>();
     encryptStub.createHmac.mockReturnValue("any_hash");
+    encryptStub.updateHasher.mockReturnValue("any_updateHasher");
     sut = new CreateUser(encryptStub);
-    process.env = { Secret: "any_secret" };
+    process.env = { Secret: "any_secret", ClientId: "any_clientId" };
   });
-  test("should call createHmac", () => {
+
+  test("should call createHmac corretly", () => {
     sut.create(dataCreateUser);
 
     expect(encryptStub.createHmac).toHaveBeenCalledTimes(1);
     expect(encryptStub.createHmac).toHaveBeenCalledWith(
-      "256",
+      "sha256",
       process.env.Secret
+    );
+  });
+
+  test("should call updateHasher corretly", () => {
+    sut.create(dataCreateUser);
+
+    expect(encryptStub.updateHasher).toHaveBeenCalledTimes(1);
+    expect(encryptStub.updateHasher).toHaveBeenCalledWith(
+      `${dataCreateUser.userName}${process.env.ClientId}`
     );
   });
 });
